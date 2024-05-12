0b5501e3d3efbaa5b7454940b02dc438872bf621
Elton Moreira <eltonms17@gmail.com>
2024-05-11 22:24:22 -0300

    1° commit
    Criação do projeto e atualizações

---
+++ Controllers/Associados.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CansadosFC.Controllers
+{
+    public class Associados : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CriarAssociado()
+        {
+            return View();
+        }
+        public IActionResult EditarAssociado()
+        {
+            return View();
+        }
+        public IActionResult ApagarAssociado()
+        {
+            return View();
+        }
+    }
+}
